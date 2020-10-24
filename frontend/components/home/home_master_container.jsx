8f297ec0172a526164d0382a4b85a6a8cf52fb59
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-10-23 17:42:24 -0700

    Added home page master container component, added it to app conponent and changed history push when signing in

---
+++ frontend/components/home/home_master_container.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class HomePageMasterContainer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div className="home-master-container-div">
+                <div className="home_page_inner_div">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default HomePageMasterContainer;

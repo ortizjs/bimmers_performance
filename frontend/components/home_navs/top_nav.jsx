2c986e6887effbf4e3431d32421e5a1147f4386f
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-10-28 21:54:56 -0700

    Started top nav for home page

--- frontend/components/home_navs/top_nav.jsx
+++ frontend/components/home_navs/top_nav.jsx
@@ -15,19 +15,53 @@ class TopNavBar extends React.Component {
     render() {
         return (
             <div className="top-nav-container">
-                <ul className="top-nav-ul">
-                    <li className="clients-li">
-                        Clients
-                    </li>
+                <div className="top-nav-dropdowns-container">
 
-                    <li className="invoices-li">
-                        Invoices
-                    </li>
+                    <div className="dropdown-button-container">
+                        <button className="dropbutton">Clients</button>
+                        <div className="dropdown-content">
+                            <a href="#">Add New client</a>
+                            <a href="#">Search Client</a>
+                            <a href="#">All Clients</a>
+                        </div>
+                    </div>
 
-                    <li className="clients-li">
-                        Vehicles
-                    </li>
-                </ul>
+                    <div className="dropdown-button-container">
+                        <button className="dropbutton">Invoices</button>
+                        <div className="dropdown-content">
+                            <a href="#">Add New Invoice</a>
+                            <a href="#">View All Invoices</a>
+                            <a href="#">Paid All Invoices</a>
+                        </div>
+                    </div>
+                    <div className="dropdown-button-container">
+                        <button className="dropbutton">Vehicles</button>
+                        <div className="dropdown-content">
+                            <a href="#">Add New Vehicle</a>
+                            <a href="#">View All Vehicles</a>
+                            <a href="#">Other</a>
+                        </div>
+                    </div>
+                    {/* <ul className="top-nav-ul">
+                        <li className="clients-li">
+                            <Link className="drop-down-link">
+                                Clients
+                            </Link>
+                        </li>
+
+                        <li className="invoices-li">
+                            <Link>
+                                Invoices
+                            </Link>
+                        </li>
+
+                        <li className="clients-li">
+                            <Link>
+                                Vehicles
+                            </Link>
+                        </li>
+                    </ul> */}
+                </div>
             </div>
         )
     }

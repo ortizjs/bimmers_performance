f44e0bf07674cb70785b727df2733a21a0d3f81c
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-10-31 12:19:50 -0700

    Added Cleints controller and methods

---
+++ app/controllers/api/clients_controller.rb
@@ -0,0 +1,33 @@
+class Api::ClientsController < ApplicationController
+
+    def new
+        @client = Client.new
+    end
+
+    def index
+        @clients = Client.all_clients
+        render :index
+    end
+
+    def show
+        @client = Client.find(params[:id])
+        render :show
+    end
+
+    def create
+        @client = Client.new(client_params)
+        @client.creator_id = current_user.id
+        if @client.save
+            render :show
+        else
+            render json: @client.errors.full_messages, status: 404
+        end
+    end
+
+    private
+
+    def client_params
+        params.require(:client).permit(:email, :first_name, :last_name, :make, 
+        :model, :year, :registration, :address, :cell_phone, :home_phone, :vin, :last_service, :creator_id)
+    end
+end

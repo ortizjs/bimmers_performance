9e3ecdec0db6c7010f5e3d56c15edc74aee8e03f
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-11-01 21:55:54 -0800

    Cleaned up ClientsReducer

--- frontend/reducers/clients_reducer.js
+++ frontend/reducers/clients_reducer.js
@@ -3,14 +3,15 @@ import { RECEIVE_CLIENTS, RECEIVE_CLIENT, REMOVE_CLIENT } from '../actions/clien
 
 const clientsReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
+    let newState;
     switch(action.type) {
         case RECEIVE_CLIENTS:
             return action.photos;
         case RECEIVE_CLIENT:
-            let newState = merge({}, oldState, {[action.client.id]: action.client});
+            newState = merge({}, oldState, {[action.client.id]: action.client});
             return newState
         case REMOVE_CLIENT:
-            let newState = merge({}, oldState);
+            newState = merge({}, oldState);
             delete newState[action.id];
             return newState;
         default:

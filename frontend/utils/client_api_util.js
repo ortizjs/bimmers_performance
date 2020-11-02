ebf345e11644943a8c0015e1cd107a0377954afa
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-11-01 22:37:24 -0800

    Added client api utils

---
+++ frontend/utils/client_api_util.js
@@ -0,0 +1,40 @@
+export const fetchClients = () => (
+  $.ajax({
+      method: "GET",
+      url: "api/clients"
+  })  
+);
+
+export const fetchClient = (id) => (
+    $.ajax({
+        method: "GET",
+        url: `api/clients/${id}`
+    })
+);
+
+export const createClient = (client) => (
+    $.ajax({
+        method: "POST",
+        url: "api/clients",
+        data: client,
+        contentType: false,
+        processData: false
+    })
+);
+
+export const updateClient = (client) => {
+    $.ajax({
+        method: "PATCH",
+        url: `api/clients/${client.id}`,
+        data: { client },
+        contentType: false,
+        processData: false
+    })
+};
+
+export const deleteClient = (id) => {
+    $.ajax({
+        method: "DELETE",
+        url: `api/clients/${id}`
+    })
+};

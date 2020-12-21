f5d5d27c70c1907c0f6e06b39844e40e96b351b7
Jonnatan Ortiz <jonnatano94@gmail.com>
2020-12-20 20:06:59 -0800

    WIP figuring out bug for client show page

--- frontend/components/clients/client_show_container.js
+++ frontend/components/clients/client_show_container.js
@@ -3,14 +3,12 @@ import { fetchClient } from '../../actions/client_actions';
 import ClientShow from './client_show';
 
 const mapStateToProps = (state, ownProps) => {
-    let client_id = ownProps.match.params.clientId;
+    // let client = state.entities.clients[client_id] || {};
+    // debugger
+    let client_id = ownProps.match.params.clientId
     let client = state.entities.clients[client_id];
-    // let car = client.cars[0]
-
     return ({
         client: client,
-        client_id: client_id,
-        // cars = car
     });
 };
 

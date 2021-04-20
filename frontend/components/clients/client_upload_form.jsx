aab84f66bd96d1d0ef76f269444ebe5b66c3f609
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-04-19 21:38:35 -0700

    Change placeHolder to palceholder in client upload form component

--- frontend/components/clients/client_upload_form.jsx
+++ frontend/components/clients/client_upload_form.jsx
@@ -42,11 +42,11 @@ class ClientUploadForm extends React.Component {
                 <form className="client-upload-form" onSubmit={this.handleSubmit}>
                     <div className="client-upload-form-input-container">
                         <h4 className="client-form-input-label">Client First Name (Required): </h4>
-                        <input className="client-upload-form-input" type="text" placeHolder="First Name" onChange={this.handleInput("first_name")} />
+                        <input className="client-upload-form-input" type="text" placeholder="First Name" onChange={this.handleInput("first_name")} />
                     </div>
                     <div className="client-upload-form-input-container">
                         <h4 className="client-form-input-label">Client Last Name (Required): </h4>
-                        <input className="client-upload-form-input" type="text" placeHolder="Last Name" onChange={this.handleInput("last_name")} />
+                        <input className="client-upload-form-input" type="text" placeholder="Last Name" onChange={this.handleInput("last_name")} />
                     </div>
                     <div className="client-upload-form-input-container">
                         <h4 className="client-form-input-label">Client Address:</h4>

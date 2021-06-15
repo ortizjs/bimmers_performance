6e124c26910094dcfba7a27b594b9a9c6117a125
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-06-14 19:38:36 -0700

    Add services reducer

---
+++ frontend/reducers/services_reducers.js
@@ -0,0 +1,22 @@
+import merge from 'lodash/merge';
+import { RECEIVE_SERVICES, RECEIVE_SERVICE, REMOVE_SERVICE} from 'actions/service_actions';
+
+const servicesReducer = (oldState = {}, actions) => {
+    Object.freeze(oldState);
+    let newState;
+    switch(action.type) {
+        case RECEIVE_SERVICES:
+            return action.services;
+        case RECEIVE_SERVICE:
+            newState = merve({}, oldState, {[action.service.id]: action.service});
+            return newState;
+        case REMOVE_SERVICE:
+            newState = merge({}, oldState);
+            delete newState[action.id];
+            return newState;
+        default:
+            return oldState
+    }
+};
+
+export default servicesReducer;

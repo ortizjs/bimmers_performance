916816ef58b491614936cab4febe30249ccef24b
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-06-14 19:39:39 -0700

    Add servicesReducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -4,13 +4,15 @@ import errorsReducer from './errors_reducer';
 import sessionReducer from './session_reducer';
 import contactUsReducer from './contact_reducer';
 import clientsReducer from './clients_reducer';
+import servicesReducer from './services_reducers';
 
 const rootReducer = combineReducers({ 
     entities: entitiesReducer,
     session: sessionReducer,
     errors: errorsReducer,
     contactus: contactUsReducer,
-    clients: clientsReducer
+    clients: clientsReducer,
+    services: servicesReducer,
 });
 
 export default rootReducer;

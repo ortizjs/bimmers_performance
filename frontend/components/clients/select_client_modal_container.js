e436915b744aaecde7ce4f221167abba9c32ad23
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-06-15 20:32:33 -0700

    add ClientSelect modal and container

---
+++ frontend/components/clients/select_client_modal_container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { fetchFilteredClients } from '../../actions/client_actions';
+import ClientSelectModal from './select_client_modal'
+
+const mapStateToProps = (state, ownProps) => {
+    let client_id = ownProps.match.params.client_id;
+    let client_params = ownProps.match.params.client_params;
+    let filteredClients = Object.values(state.entities.filteredClients)
+    return ({
+        filteredClients: filteredClients
+    });
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return({
+        fetchFilteredClients: () => dispatch(fetchFilteredClients())
+    });
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ClientSelectModal));

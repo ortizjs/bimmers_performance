6a90383e899153e53fd53de4ed5fb24dfc45f633
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-06-16 21:31:18 -0700

    Add more code for select client modal as well as handle change function for rendering clients to select from

--- frontend/components/clients/select_client_modal.jsx
+++ frontend/components/clients/select_client_modal.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
+import ClientItem from './client_item';
 
 class SelectClientModal extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            searchParams: ""
+        };
+        this.updateSearch = this.updateSearch.bind(this);
+    }
+
+    updateSearch(e) {
+        e.preventDefault();
+        // let searchParams = {}
+        // debugger
+        var input = e.target.value.substr(0, 40)
+        this.setState({searchParams: input}, () => {
+            console.log(this.state)
+            this.props.fetchFilteredClients(input)
+        })
     }
     
     render() {
+        let filteredClients = this.props.clients;
+
         return (
             <div className="client-select-modal-div">
-                <input className="client-search-bar-input"
-                        type="text"
-                        placeholder="Search Client"/>
-                <button className="client-select-close-modal-button" onClick={this.props.handleClientModal}>Select Client</button>
+                <div className="client-search-bar-continer">
+                    <input className="client-search-bar-input"
+                            type="text"
+                            placeholder="Search Client"
+                            onChange={this.updateSearch}/>
+                    <button className="client-select-close-modal-button" onClick={this.props.handleClientModal}>Add</button>
+
+                    <ul>
+                        {filteredClients.map((client) => {
+                            return (
+                            <ClientItem
+                            clientFirstName="Jonnatan"
+                            clientLastName="Ortiz"
+                            clientVehice="BMW"
+                            />
+                            )
+                        })}
+                    </ul>
+                </div>
             </div>
         )
     }

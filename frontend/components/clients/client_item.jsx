b43024ffadd5e0f58b46842bcbdd8ba2d541ae0f
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-06-18 16:03:55 -0700

    change code in client item jsx file to render client information - WIP

--- frontend/components/clients/client_item.jsx
+++ frontend/components/clients/client_item.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const ClientItem = props => {
+    let client = props.client
     return (
         <div className="client-item-div">
-            <h5>{props.clientFirstName}</h5>
-            <h5>{props.clientLastName}</h5>
-            <h5>{props.clientVehicle}</h5>
+            <h5>{`${client.id}  ${client.first_name}  ${client.last_name}  ${client.cell_phone}  ${client.make}  ${client.model}  ${client.year}  ${client.last_service}`}</h5>
         </div>
     )
 };

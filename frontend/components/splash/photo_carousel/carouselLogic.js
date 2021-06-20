8d29057314823a1065c4107349b9f4b94cc7c80b
Jonnatan Ortiz <jonnatano94@gmail.com>
2021-06-19 21:22:58 -0700

    Removed commented code out from photo_carousel carousel logic file -- later to remove it all

--- frontend/components/splash/photo_carousel/carouselLogic.js
+++ frontend/components/splash/photo_carousel/carouselLogic.js
@@ -1,23 +1,4 @@
 export default function carouselLogic(slideIndex) {
-    //  var i;
-    //  var showSlides = document.getElementsByClassName("slides");
-    //  var dots = document.getElementsByClassName("dot");
-    //  for (i = 0; i < showSlides.length; i++) {
-    //      // debugger
-    //      showSlides[i].style.display = "none";
-    //  }
-    //  debugger
-    //  slideIndex++;
-    //  if (slideIndex > showSlides.length) {
-    //      slideIndex = 1;
-    //  }
-    //  for (i = 0; i < dots.length; i++) {
-    //      dots[i].className = dots[i].className.replace(" active", "");
-    //  }
-    //  debugger
-    //  showSlides[slideIndex - 1].style.display = "block";
-    //  dots[slideIndex - 1].className += " active";
-    //  setTimeout(carouselLogic(slideIndex), 2000); // Change image every 2 seconds
 
         var metaslider_25 = function($) {
             $('#metaslider_25').addClass('flexslider'); // theme/plugin conflict avoidance
